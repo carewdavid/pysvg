e70f85eb6eabc65de612f4c119154bbbf3524584
david <david@dcarew.com>
2021-03-08 09:53:28 -0800

    Add methods for other shapes to SVG

--- SVG.py
+++ SVG.py
@@ -1,4 +1,7 @@
 from . import Circle
+from . import Rect
+from . import Ellipse
+from . import Line
 
 class SVG:
     def __init__(self, width, height):
@@ -9,6 +12,21 @@ class SVG:
     def circle(self, x, y, radius):
         circ = Circle.Circle(x, y, radius)
         self.objs.append(circ)
+
+    def rect(self, x, y, w, h):
+        rect = Rectangle.Rectangle(x, y, w, h)
+        self.objs.append(rect)
+        return rect
+
+    def Ellipse(self, x, y, rx, ry):
+        ell = Ellipse.Ellipse(x, y, rx, ry)
+        self.objs.append(ell)
+        return ell
+
+    def Line(self, sx, sy, ex, ey):
+        line = Line.Line(sx, sy, ex, ey)
+        self.objs.append(line)
+        return line
 
     def draw(self):
         print('<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}">'.format(self.width, self.height))
