c215d1e1fefd63ef6de693abd605afa31db8519a
david <david@dcarew.com>
2021-03-22 20:35:07 -0700

    Add Path move commands

---
+++ Path.py
@@ -0,0 +1,14 @@
+class Path:
+    def __init__(self, x, y):
+       self.commands = [] 
+       self.moveTo(x, y)
+
+    def move(self, x, y):
+        """Move relative to the last point"""
+        self.commands.append(f'm {x} {y}')
+        return self
+
+    def move(self, x, y):
+        """Move to the given point"""
+        self.commands.append(f'M {x} {y}')
+        return self
