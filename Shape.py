d3eff8d120b0faf956d1274fe26139595fa054c7
David Carew <david@dcarew.com>
2021-05-06 19:28:17 -0700

    Return the shape from property setters so they can be chained

--- Shape.py
+++ Shape.py
@@ -5,12 +5,15 @@ class Shape:
 
     def fill(self, fillColor):
         self.attrs["fill"] = fillColor
+        return self
 
     def stroke(self, strokeColor):
         self.attrs["stroke"] = strokeColor
+        return self
 
     def strokeWidth(self, strokeWidth):
         self.attrs["stroke-width"] = strokeWidth
+        return self
 
     def props(self):
         return " ".join([f'{k}="{v}"' for k, v in self.attrs.items()])
